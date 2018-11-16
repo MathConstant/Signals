dc9667c5553f55814bc41be55dcaad0427aabf50
MathConstant <matthias.ricker@gmail.com>
2018-11-15 21:01:25 -0500

    fixed the work that was lost (i think i might have forgot about somthing)

--- MattlabAs2.m
+++ MattlabAs2.m
@@ -1,25 +1,42 @@
-function Xw =  MyFT(Xt, t, w)
-kr = 500;
-pT= (2*pi)/wo;
-a = zeros(1,kr);   %initializes an array for a with required size
+t = 1/16000;
 
-% calculate a for different k values (this uses -50:50)
-%for k = 1:1:kr 
-   po = @(t) exp(-1j*(2*pi/pT)* t);
-   sum = 0;
-   %Integral approximation
-   for k = 1:1:kr  %for t = 0.1:0.1:pT 
-       presum = @(t) (Xt(t) .* po(t)); 
-       sum = sum + 0.1*presum(t); 
-   end
-   a(k) = sum; 
-end
-% a repsesents the fourier coeffecents from k = -50:50   
-Xw = a
+Xw = MyFT(Xt, t, w);
+Xt = MyiFT(Wt, w, t);
+
+function Xw =  MyFT(Xt, t, w)	
+	% t is time bettween samples 
+	% Xt is an array 
+	% w is fundemental frequency
+	ak = fCoeff(Xt,wo,t)
+	Xw = 2*pi .* ak
 end
 
 function Xt =  MyiFT(Wt, w, t)
-
+	%t currently not used ether
+	xt = @(t) 0*t;
+	%sum 
+	for k = 1:1:length(Wt) %not certain about how to make this an integral in terms of w
+		ipo = @(t) exp(1j*(w)*t);
+		xt = @(t) (xt(t) +  Wt(k)*ipo(t));
+	end
+	Xt = xt/(2*pi)
 end
-Xw = MyFT(Xt, t, w);
-Xt = MyiFT(Wt, w, t);
+
+
+function ak = fCoeff(fun,wo,sT)
+pT= (2*pi)/wo;
+kr = length(fun)
+a = zeros(1,kr);   %initializes an array for a with required size
+
+for k = 1:1:kr 
+   po = @(t) exp(-1j*(k - (kr/2 +1))*(2*pi/pT)* t);
+   sum = 0;
+   %Integral approximation
+   for t = sT:sT:pT/sT
+       presum = (fun(t* (1/sT)) * po(t))/pT; 
+       sum = sum + sT*presum; 
+   end
+   a(k) = sum; 
+end
+ak = a
+end
