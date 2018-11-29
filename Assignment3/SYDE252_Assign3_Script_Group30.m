f49b35c3c805b1bccf31a3da905463c7412034e7
Neil Brubacher <31701193+neilbrub@users.noreply.github.com>
2018-11-29 17:22:30 -0500

    Add files via upload

--- Assignment3/SYDE252_Assign3_Script_Group30.m
+++ Assignment3/SYDE252_Assign3_Script_Group30.m
@@ -20,30 +20,30 @@ sound(newY,sr);
 % Comment on your observation
 
 % Build time-domain
-length_original = length(y);
-length_synthesized = length(newY);
-
-tDomain_original = 0:sr:(length_original-1) * sr;
-tDomain_synthesized = 0:sr:(length_synthesized-1) * sr;
-
-% specify frequency domain to inspect with FT
-w = -1000:1000;
-fDomain_length = length(w);
-
-result_original = zeros(1, fDomain_length);
-for i = 1:fDomain_length
-    result_original(i) = MyFT(y, tDomain_original, w(i));
-end
-
-result_synthesized = zeros(1, fDomain_length);
-for j = 1:fDomain_length
-    result_synthesized(j) = MyFT(newY, tDomain_synthesized, w(j));
-end
-
-plot(w, result_original)
-hold on
-plot(w, result_synthesized)
-hold off
+% length_original = length(y);
+% length_synthesized = length(newY);
+% 
+% tDomain_original = 0:sr:(length_original-1) * sr;
+% tDomain_synthesized = 0:sr:(length_synthesized-1) * sr;
+% 
+% % specify frequency domain to inspect with FT
+% w = -1000:1000;
+% fDomain_length = length(w);
+% 
+% result_original = zeros(1, fDomain_length);
+% for i = 1:fDomain_length
+%     result_original(i) = MyFT(y, tDomain_original, w(i));
+% end
+% 
+% result_synthesized = zeros(1, fDomain_length);
+% for j = 1:fDomain_length
+%     result_synthesized(j) = MyFT(newY, tDomain_synthesized, w(j));
+% end
+% 
+% plot(w, result_original)
+% hold on
+% plot(w, result_synthesized)
+% hold off
 
 % end of Question 1
 %% Changing the speed of the speech, without significantly modifying the tone of the speech
@@ -52,12 +52,31 @@ sound(y,sr/2);
 % play the audio at double speed
 sound(y,sr*2);
 % Question 2
-%I think this might be Time Expansion
+% The property that explains why tone is lowered when the audio is played at half speed is the time-scaling property.  
+% According to this property, when a function is expanded in time by a factor a "a", its Fourier Transform is compressed in frequency by “a”. 
+% 
+% x(t/a) -> aX(?a)
+% 
+% Lower frequencies have a lower pitch therefore an increases in time will make the output signal sound lower and vice versa.
 
 % end of Question 2
+
+
 % Question 3: changing the length of x, without changing its general shape
-x_f = interp1(); % finish this line to generate x_f[n] for faster speech
-x_s = interp1(); % finish this line to generate x_s[n] for slower speech
+
+xqf = 1:2:length(x)*2;
+x_f = interp1(x, xqf); % finish this line to generate x_f[n] for faster speech
+
+% Since the slow signal takes twice the domain as x, we perform two interps
+% to ensure we don't lose the second half
+xqs1 = 1:0.5:length(x)*0.5;
+x_s1 = interp1(x, xqs1); % finish this line to generate x_s[n] for slower speech
+
+xqs2 = length(x)*0.5:0.5:length(x);
+x_s2 = interp1(x, xqs2);
+
+x_s = [x_s1, x_s2];
+
 % end of Question 3
 
 % synthesis voices using the x_fast and x_slow you just generated
